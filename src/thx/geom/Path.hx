09b26cc978a2f4401970ff13da81ef1181bcc202
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-08-10 23:18:53 -0600

    added notes to path

--- src/thx/geom/Path.hx
+++ src/thx/geom/Path.hx
@@ -51,6 +51,53 @@ class Path {
 		}
 	}
 
+//	public function transform(matrix : Matrix4x4) {
+//		var ismirror = matrix.isMirroring(),
+//			result = new Path(sides.map(function(side) {
+//				return side.transform(matrix);
+//			}), closed);
+//		if(ismirror)
+//			result = result.flipped();
+//		return result;
+//	}
+
+//	public function getArea() {
+//		var area = 0.0;
+//		for(side in sides)
+//			area += side.vertex0.position.cross(side.vertex1.position);
+//		return area / 2;
+//	}
+
+//	public function flip() {
+//		var arr = sides.map(function(side) return side.flip());
+//		arr.reverse();
+//		return new Path(att, closed);
+//	}
+
+//	public function isSelfIntersecting() {
+//		var length = sides.length,
+//			side0, side1;
+//		for(i in 0...length) {
+//			side0 = sides[i];
+//			for(j in i + 1...length) {
+//				side1 = sides[j];
+//				if(side1.intersects(side0)) {
+//					return true;
+//				}
+//			}
+//		}
+//	}
+
+//	public function isPolygon() {
+//		for(side in sides)
+//			if(!side.isLinear())
+//				return false;
+//		return true;
+//	}
+
+//	private function get_length()
+//		return null != length ? length : length = sides.fold(function(side, len) return len + side.length, 0.0);
+
 	public function toString() {
 		return 'Path(${nodes.map(function(n) return "["+n.toStringValues()+"]").join(", ")},$closed)';
 	}
