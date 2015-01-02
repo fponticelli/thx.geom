9cfc034d52045c09158f8a489e3fe9a5f45a8e1b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-02 12:05:29 -0700

    fixed basics

--- src/thx/geom/Matrix2x3.hx
+++ src/thx/geom/Matrix2x3.hx
@@ -1,9 +1,9 @@
 package thx.geom;
 
 using thx.core.Arrays;
-import thx.geom.m;
+import thx.geom.m.*;
 
 abstract Matrix2x3(M2x3) {
-  public inline function(m : M2x3)
+  public inline function new(m : M2x3)
     this = m;
 }
