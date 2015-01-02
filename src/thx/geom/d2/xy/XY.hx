337502cdaa04fd1cc310ac42241c0abbeb342835
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-02 16:13:30 -0700

    XY and XYZ now implement ITransformable44

--- src/thx/geom/d2/xy/XY.hx
+++ src/thx/geom/d2/xy/XY.hx
@@ -1,6 +1,8 @@
 package thx.geom.d2.xy;
 
-interface XY {
+import thx.geom.ITransformable44;
+
+interface XY extends ITransformable44<XY> {
   public var x(get, set) : Float;
   public var y(get, set) : Float;
 }
--- src/thx/geom/d3/xyz/XYZ.hx
+++ src/thx/geom/d3/xyz/XYZ.hx
@@ -2,6 +2,6 @@ package thx.geom.d3.xyz;
 
 import thx.geom.d2.xy.XY;
 
-interface XYZ extends XY {
+interface XYZ extends XY extends ITransformable44<XYZ> {
   public var z(get, set) : Float;
 }
