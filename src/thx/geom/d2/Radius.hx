c097fc5478e09ab009841e370c5d1e1c7ee69286
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-20 19:11:52 -0600

    added Radius and empty test

---
+++ src/thx/geom/d2/Radius.hx
@@ -0,0 +1,49 @@
+package thx.geom.d2;
+
+using thx.core.Floats;
+import thx.geom.core.*;
+
+abstract Radius(Dim) from Dim to Dim {
+  inline public static function create(r : Float) : Radius
+    return new MutableDim(Math.abs(r));
+
+  inline public static function linked(getRadius : Void -> Float, setRadius : Float -> Float) : Radius
+    return new LinkedDim(getRadius, setRadius);
+
+  inline public static function immutable(r : Float) : Radius
+    return new ImmutableDim(r);
+
+  public var coord(get, set) : Float;
+
+  inline public function new(r : Dim)
+    this = r;
+
+  @:op(A==B)
+  inline public function equals(p : Radius)
+    return coord == p.coord;
+
+  @:op(A!=B)
+  inline public function notEquals(p : Radius)
+    return !equals(p);
+
+  public function nearEquals(p : Radius)
+    return coord.nearEquals(p.coord);
+
+  inline public function notNearEquals(p : Radius)
+    return !nearEquals(p);
+
+  inline public function isZero()
+    return coord == 0;
+
+  public function isNearZero()
+    return coord.nearZero();
+
+  inline public function clone() : Radius
+    return this.clone();
+
+  @:to inline public function toString()
+    return 'Radius($coord)';
+
+  inline function get_coord() return this.coord;
+  inline function set_coord(v : Float) return this.coord = v;
+}
---
+++ test/thx/geom/d2/TestRadius.hx
@@ -0,0 +1,13 @@
+package thx.geom.d2;
+
+import thx.geom.d2.*;
+import thx.geom.core.*;
+import utest.Assert;
+
+class TestRadius {
+  public function new() {}
+
+  public function testBasics() {
+    var r = Radius.create(5);
+  }
+}
