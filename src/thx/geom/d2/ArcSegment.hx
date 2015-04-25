96ac76f188fa1e067b523d38232369b1402b1de1
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-24 21:40:12 -0600

    added ArcSegment

---
+++ src/thx/geom/d2/ArcSegment.hx
@@ -0,0 +1,20 @@
+package thx.geom.d2;
+
+import thx.geom.core.Dim;
+
+class ArcSegment extends LineSegment {
+  public var radius(default, null) : Vector;
+  public var largeArcFlag : Bool;
+  public var sweepFlag : Bool;
+  public var xAxisRotate(default, null) : Dim;
+  public function new(start : Point, radius : Vector, largeArcFlag : Bool, sweepFlag : Bool, end : Point, xAxisRotate : Dim) {
+    super(start, end);
+    this.radius = radius;
+    this.largeArcFlag = largeArcFlag;
+    this.sweepFlag = sweepFlag;
+    this.xAxisRotate = xAxisRotate;
+  }
+
+  override public function toString()
+    return 'ArcSegment(sx:${start.x},sy:${start.y},xr:${xAxisRotate},laf:${largeArcFlag},sf:${sweepFlag},ex:${end.x},ey:${end.y})';
+}
