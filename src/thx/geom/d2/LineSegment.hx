3460b3f004c8f30b803d82ee8d73cc6774919e99
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-24 21:33:19 -0600

    LineSegment.toVector

--- src/thx/geom/d2/LineSegment.hx
+++ src/thx/geom/d2/LineSegment.hx
@@ -8,6 +8,20 @@ class LineSegment implements ISegment {
     this.end = end;
   }
 
+  public function toVector()
+    return Vector.linked(
+      function( ) return end.x - start.x,
+      function( ) return end.y - start.y,
+      function(v) {
+        end.x = start.x + v;
+        return v;
+      },
+      function(v) {
+        end.y = start.y + v;
+        return v;
+      }
+    );
+
   public function toString()
     return 'LineSegment(${start.x},${start.y},${end.x},${end.y})';
 }
