df7aeb9361241beb09b40e899bb504f55eb169e4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-26 23:13:06 -0600

    added simple test path from/to SVG

--- test/thx/geom/d2/TestPath.hx
+++ test/thx/geom/d2/TestPath.hx
@@ -7,8 +7,9 @@ import utest.Assert;
 class TestPath {
   public function new() {}
 
-  public function testBasics() {
-    var path = new Path();
-    trace(path.toString());
+  public function testParseSVGPath() {
+    var d = "M 80 80 A 45 45 0 0 0 125 125 L 125 80 Z",
+        path = Path.fromSVGPath(d);
+    Assert.equals(d, path.toSVGPath());
   }
 }
