9b72a4086347b42585a5b422ef270af329a912cb
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-27 21:25:45 -0600

    added test for closing paths before the end

--- test/thx/geom/d2/TestPath.hx
+++ test/thx/geom/d2/TestPath.hx
@@ -8,7 +8,13 @@ class TestPath {
   public function new() {}
 
   public function testParseSVGPath() {
-    var d = "M 80 80 A 45 45 0 0 0 125 125 L 125 80 Z",
+    var d = "M 80 80 A 45 45 0 0 0 125 125 L 125 80 L 80 80 Z",
+        path = Path.fromSVGPath(d);
+    Assert.equals(d, path.toSVGPath());
+  }
+
+  public function testParseSVGPathMultiClosing() {
+    var d = "M 100 100 L 150 20 L 200 100 L 100 100 Z M 250 100 L 300 20 L 350 100 L 250 100 Z",
         path = Path.fromSVGPath(d);
     Assert.equals(d, path.toSVGPath());
   }
