5f3f859f36e0ec06f7416b99dc1b6a65bab489e1
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-03 20:05:17 -0600

    added Matrix23.scaled

--- src/thx/geom/Matrix23.hx
+++ src/thx/geom/Matrix23.hx
@@ -18,6 +18,9 @@ abstract Matrix23(M23) from M23 to M23 {
   static public function rotationAt(center : Point, radians :  Float)
     return identity.translateBy(-center).rotate(radians).translateBy(center);
 
+  static public function scaled(scaleFactorX : Float, ?scaleFactorY : Float) : Matrix23
+    return Matrix23.create(scaleFactorX, 0, 0, null == scaleFactorY ? scaleFactorX : scaleFactorY, 0, 0);
+
   static public function translation(x : Float, y : Float)
     return Matrix23.create(1, 0, 0, 1, x, y);
 
