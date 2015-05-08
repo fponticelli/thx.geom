cca42dc52a0e1fd7f9bf47e9cd86f368136a0cf9
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-08 00:37:54 -0600

    round path coords in SVG export

--- src/thx/geom/d2/Path.hx
+++ src/thx/geom/d2/Path.hx
@@ -2,6 +2,7 @@ package thx.geom.d2;
 
 import thx.geom.d2.svg.Svg;
 using thx.Arrays;
+using thx.Floats;
 
 class Path implements IShape {
   public static function fromSVGPath(d : String)
@@ -23,21 +24,21 @@ class Path implements IShape {
 
     for(segment in segments) {
       if(!end.equals(segment.start)) {
-        buf.push('M ${segment.start.x} ${segment.start.y}');
+        buf.push('M ${r(segment.start.x)} ${r(segment.start.y)}');
         startingPoint = segment.start;
       }
       switch Type.getClass(segment) {
         case LineSegment:
-          buf.push('L ${segment.end.x} ${segment.end.y}');
+          buf.push('L ${r(segment.end.x)} ${r(segment.end.y)}');
         case QuadraticCurveSegment:
           var s : QuadraticCurveSegment = cast segment;
-          buf.push('Q ${s.c1.x} ${s.c1.y} ${s.end.x} ${s.end.y}');
+          buf.push('Q ${r(s.c1.x)} ${r(s.c1.y)} ${r(s.end.x)} ${r(s.end.y)}');
         case CubicCurveSegment:
           var s : CubicCurveSegment = cast segment;
-          buf.push('C ${s.c1.x} ${s.c1.y} ${s.c2.x} ${s.c2.y} ${s.end.x} ${s.end.y}');
+          buf.push('C ${r(s.c1.x)} ${r(s.c1.y)} ${r(s.c2.x)} ${r(s.c2.y)} ${r(s.end.x)} ${r(s.end.y)}');
         case ArcSegment:
           var s : ArcSegment = cast segment;
-          buf.push('A ${s.radius.x} ${s.radius.y} ${s.xAxisRotate.degrees} ${s.largeArcFlag ? 1 : 0} ${s.sweepFlag ? 1 : 0} ${s.end.x} ${s.end.y}');
+          buf.push('A ${r(s.radius.x)} ${r(s.radius.y)} ${r(s.xAxisRotate.degrees)} ${s.largeArcFlag ? 1 : 0} ${s.sweepFlag ? 1 : 0} ${r(s.end.x)} ${r(s.end.y)}');
       }
       if(segment.end.equals(startingPoint)) {
         buf.push("Z");
@@ -47,6 +48,9 @@ class Path implements IShape {
 
     return buf.join(" ");
   }
+
+  static function r(v : Float)
+    return v.roundTo(9);
 
   public function toString()
     return 'Path(segments=${segments.length})';
