2e53db1fee7c26e4f09f79ecedd780ea7c1d417d
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-04-29 11:32:44 -0600

    fixes for dev compiler wrong behavior when assigning a value when used as a function parameter

--- src/thx/geom/d2/svg/Svg.hx
+++ src/thx/geom/d2/svg/Svg.hx
@@ -30,7 +30,7 @@ class Svg {
 
     function capturePoints(qt : Int) {
       var c = capture(qt * 2);
-      return c.splitBy(2).map.fn(Point.create(_[0], _[1]));
+      return c.splitBy(2).map(function(p) return Point.create(p[0], p[1]));
     }
 
     function capturePoint()
@@ -65,102 +65,126 @@ class Svg {
           prev = "L";
           smooth = null;
           var p = capturePoint();
-          list.push(new LineSegment(last, last = p));
+          list.push(new LineSegment(last, p));
+          last = p;
         case "l":
           prev = "l";
           smooth = null;
           var p = capturePoint();
-          list.push(new LineSegment(last, last = last + p));
+          list.push(new LineSegment(last, last + p));
+          last = last + p;
         case "Q":
           prev = "Q";
           var ps = capturePoints(2);
-          list.push(new QuadraticCurveSegment(last, smooth = ps[0], last = ps[1]));
+          list.push(new QuadraticCurveSegment(last, ps[0], ps[1]));
+          smooth = ps[0];
+          last = ps[1];
         case "q":
           prev = "q";
           var ps = capturePoints(2);
-          list.push(new QuadraticCurveSegment(last, smooth = last + ps[0], last = last + ps[1]));
+          list.push(new QuadraticCurveSegment(last, last + ps[0], last + ps[1]));
+          smooth = last + ps[0];
+          last = last + ps[1];
         case "C":
           prev = "C";
           var ps = capturePoints(3);
-          list.push(new CubicCurveSegment(last, ps[0], smooth = ps[1], last = ps[2]));
+          list.push(new CubicCurveSegment(last, ps[0], ps[1], ps[2]));
+          smooth = ps[1];
+          last = ps[2];
         case "c":
           prev = "c";
           var ps = capturePoints(3);
-          list.push(new CubicCurveSegment(last, last + ps[0], smooth = last + ps[1], last = last + ps[2]));
+          list.push(new CubicCurveSegment(last, last + ps[0], last + ps[1], last + ps[2]));
+          smooth = last + ps[1];
+          last = last + ps[2];
         case "H":
           prev = "H";
           smooth = null;
           var x = captureOne(),
               p = last.clone();
           p.x = x;
-          list.push(new LineSegment(last, last = p));
+          list.push(new LineSegment(last, p));
+          last = p;
         case "h":
           prev = "h";
           smooth = null;
           var x = captureOne(),
               p = last.clone();
           p.x += x;
-          list.push(new LineSegment(last, last = p));
+          list.push(new LineSegment(last, p));
+          last = p;
         case "V":
           smooth = null;
           prev = "V";
           var y = captureOne(),
               p = last.clone();
           p.y = y;
-          list.push(new LineSegment(last, last = p));
+          list.push(new LineSegment(last, p));
+          last = p;
         case "v":
           smooth = null;
           prev = "v";
           var y = captureOne(),
               p = last.clone();
           p.y += y;
-          list.push(new LineSegment(last, last = p));
+          list.push(new LineSegment(last, p));
+          last = p;
         case "S":
           prev = "S";
           var ps = capturePoints(2);
           smooth = null != smooth ? smooth.reflection(last) : ps[0];
-          list.push(new CubicCurveSegment(last, smooth, smooth = ps[0], last = ps[1]));
+          list.push(new CubicCurveSegment(last, smooth, ps[0], ps[1]));
+          smooth = ps[0];
+          last = ps[1];
         case "s":
           smooth = null;
           var ps = capturePoints(2);
           smooth = null != smooth ? smooth.reflection(last) : last + ps[0];
-          list.push(new CubicCurveSegment(last, smooth, smooth = last + ps[0], last = last + ps[1]));
+          list.push(new CubicCurveSegment(last, smooth, last + ps[0], last + ps[1]));
+          smooth = last + ps[0];
+          last = last + ps[1];
         case "T":
           smooth = null;
           prev = "T";
           var p = capturePoint();
           smooth = null != smooth ? smooth.reflection(last) : last;
-          list.push(new QuadraticCurveSegment(last, smooth, last = p));
+          list.push(new QuadraticCurveSegment(last, smooth, p));
+          last = p;
         case "t":
           smooth = null;
           prev = "t";
           var p = capturePoint();
           smooth = null != smooth ? smooth.reflection(last) : last;
-          list.push(new QuadraticCurveSegment(last, smooth, last = last + p));
+          list.push(new QuadraticCurveSegment(last, smooth, last + p));
+          last = last + p;
         case "A":
           smooth = null;
           prev = "A";
-          var a = capture(7);
+          var a = capture(7),
+              f = Point.create(a[5], a[6]);
           list.push(new ArcSegment(
             last,
             Vector.create(a[0], a[1]),
             a[3] != 0,
             a[4] != 0,
             new MutableDim(a[2]),
-            last = Point.create(a[5], a[6])
+            f
           ));
+          last = f;
         case "a":
           smooth = null;
           prev = "a";
-          var a = capture(7);
+          var a = capture(7),
+              f = last + Point.create(a[5], a[6]);
           list.push(new ArcSegment(
             last,
             Vector.create(a[0], a[1]),
             a[3] != 0,
             a[4] != 0,
             new MutableDim(a[2]),
-            last = last + Point.create(a[5], a[6])
+            f
           ));
+          last = f;
         case "z", "Z":
           smooth = null;
           var first = list[beginShape];
--- test/thx/geom/d2/TestPath.hx
+++ test/thx/geom/d2/TestPath.hx
@@ -3,6 +3,7 @@ package thx.geom.d2;
 import thx.geom.d2.*;
 import thx.geom.core.*;
 import utest.Assert;
+using thx.Iterables;
 
 class TestPath {
   public function new() {}
@@ -17,5 +18,12 @@ class TestPath {
     var d = "M 100 100 L 150 20 L 200 100 L 100 100 Z M 250 100 L 300 20 L 350 100 L 250 100 Z",
         path = Path.fromSVGPath(d);
     Assert.equals(d, path.toSVGPath());
+  }
+
+  public function testIssue20160429() {
+    var d = "M 0 9 L 9 0 18 0 0 18 Z M 9 18 L 18 9 18 18 Z",
+        path = Path.fromSVGPath(d),
+        od = path.toSVGPath();
+    Assert.equals(od, Path.fromSVGPath(od).toSVGPath());
   }
 }
